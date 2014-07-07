ce296472da27853667b454127fa5aabee0145f63
dyep49 <dyep49@gmail.com>
2014-07-07 12:07:05 -0400

    init commit

--- AlarmClockTest/ViewController.swift
+++ AlarmClockTest/ViewController.swift
@@ -9,9 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var alarmText : UILabel
+    
+    @IBAction func alarmTest(sender : AnyObject) {
+        alarmText.text = "Button was clicked"
+//        var alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.Alert)
+//        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+//        self.presentViewController(alert, animated: true, completion: nil)
+        setNotification()
+    }
+    
+    
+    var currentDate = ""
+    
+//    func createTime() -> String {
+//        var dateTest = NSDate()
+//        var dateFormatter = NSDateFormatter()
+//        dateFormatter.dateFormat = "yyyy-MM-dd 'at' HH:mm:ss"
+//        return dateFormatter.stringFromDate(NSDate())
+//    }
+    
+//    func update() {
+//        var updateTime = createTime()
+//        timeText.text = createTime()
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +45,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func setNotification() {
+        var localNotification = UILocalNotification()
+        localNotification.alertBody = "ALERT TEST"
+        localNotification.fireDate = NSDate().dateByAddingTimeInterval(3)
+        localNotification.soundName = "alarm.wav"
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+    }
+
+    
+
+    
 
 
 }
